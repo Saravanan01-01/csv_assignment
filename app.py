4b54f018b794a469f4a845e0d48f7f10aa1e09fd
Saravanan01-01 <18eucs099@skcet.ac.in>
2021-05-01 14:51:51 +0530

    push

--- app.py
+++ app.py
@@ -18,18 +18,18 @@ def index():
     return render_template('index.html')
 
 
-df_eff_memory = pd.read_csv('./static/aaa.csv',sep=',',dtype={'Month':'string','Average':'string','Mark':'string'})
+df_eff_memory = pd.read_csv('./static/aaa.csv',sep=',',dtype={'Id':'string','Work':'string','Name':'string'})
 df_eff_memory.memory_usage(index=False,deep=True)
 
 mydb=mysql.connector.connect(host='localhost',user='root',password='saro2001@1.',database='movie')
 cur=mydb.cursor()
-measurement_insert_query ="""insert into aaa (Month,Average,Mark) values(%s,%s,%s)"""
-data = list(zip(df_eff_memory['Month'],df_eff_memory['Average'],df_eff_memory['Mark']))
+measurement_insert_query ="""insert into employee (Id,Work,Name) values(%s,%s,%s)"""
+data = list(zip(df_eff_memory['Id'],df_eff_memory['Work'],df_eff_memory['Name']))
 
-for chunk in pd.read_csv('./static/aaa.csv',sep=',',dtype={'Month':'string','Average':'string','Mark':'string'},chunksize=10000):
+for chunk in pd.read_csv('./static/aaa.csv',sep=',',dtype={'Id':'string','Work':'string','Name':'string'},chunksize=10000):
     
     # zip the data
-    data = list(zip(chunk['Month'].to_numpy(),chunk['Average'].to_numpy(),chunk['Mark'].to_numpy()))
+    data = list(zip(chunk['Id'].to_numpy(),chunk['Work'].to_numpy(),chunk['Name'].to_numpy()))
     
     # insert data into db
     try:
